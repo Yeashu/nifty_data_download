a7ca195ae7818f31a92f38c8282639c4baaa3478
Yeashu <yeashusemwal@gmail.com>
2023-07-13 17:21:12 +0530

    refacor code remove user input functionality modify to just be used as functions

--- scraper5p.py
+++ scraper5p.py
@@ -1,111 +1,109 @@
 import sys
 import os
-
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(parent_dir)
 import datetime
 import pandas as pd
-from ApiKeys.secrets5p import (
-    APP_NAME,
-    APP_SOURCE,
-    PASSWORD,
-    ENCRYPTION_KEY,
-    USER_ID,
-    USER_KEY,
-    client_code,
-    Pin,
-)
+
 from lib.FivePaisaHelperLib import FivePaisaWrapper
 from scraper import download_stock_data, save_to_csv
 from stocksList import nifty50_stocks
 
-app = FivePaisaWrapper(
-    APP_NAME=APP_NAME,
-    APP_SOURCE=APP_SOURCE,
-    USER_KEY=USER_KEY,
-    USER_ID=USER_ID,
-    PASSWORD=PASSWORD,
-    ENCRYPTION_KEY=ENCRYPTION_KEY,
-    client_code=client_code,
-    pin=Pin,
-)
 
-def UpdateYfData():
-    dData = {}
-    for stock in nifty50_stocks:
-       symbol = stock + '.NS'
-       dData[stock] = download_stock_data(symbol,None,None,) 
-    path = '/home/yeashu/project/AlgoTrading app/nifty_data_download/Data/Equities_csv/daily/nifty50'
-    histdata = {}
-    for stock in nifty50_stocks:
-        df = pd.read_csv(f'{path}/{stock}.csv',index_col='Date')
-        histdata[stock] = df
+def load_dataframe_from_csv(symbol: str, path: str, index_col: str = 'Date') -> pd.DataFrame:
+    """
+    Load a DataFrame from a CSV file.
+
+    Args:
+        symbol (str): The symbol or stock name.
+        path (str): The path to the CSV file.
+        index_col (str, optional): The column to use as the index. Default is 'Date'.
+
+    Returns:
+        pd.DataFrame: The loaded DataFrame from the CSV file.
+    """
+    try:
+        return pd.read_csv(f'{path}/{symbol}.csv', index_col=index_col)
+    except FileNotFoundError:
+        return pd.DataFrame()
+
+
+def UpdateYfData(save_location: str, data_read_location: str) -> None:
+    """
+    Update Yahoo Finance data for Nifty 50 stocks and save it to CSV files.
+
+    Args:
+        save_location (str): The location to save the updated data.
+        data_read_location (str): The location to read the existing data.
+
+    Returns:
+        None
+    """
+    histdata = {stock: load_dataframe_from_csv(stock, data_read_location) for stock in nifty50_stocks}
+    dData = {stock: download_stock_data(stock + '.NS', None, None) for stock in nifty50_stocks}
     for stock, values in dData.items():
-        histdata[stock] = pd.concat([histdata[stock],values])
-    
-    for symbol,df in histdata.items():
-        save_to_csv(df=df,symbol=symbol,filepath=path)
-    
-def UpdateData(totp:str,intraday:bool=True,updateFrom:datetime.datetime=None):
+        histdata[stock] = pd.concat([histdata[stock], values])
+    for symbol, df in histdata.items():
+        save_to_csv(df=df, symbol=symbol, filepath=save_location)
+
+
+def UpdateData(
+    app: FivePaisaWrapper,
+    totp: str,
+    save_location: str,
+    data_read_location: str,
+    intraday: bool = True,
+    updateFrom: datetime.datetime = None
+) -> dict:
+    """
+    Update data for Nifty 50 stocks using the 5paisa API and save it to CSV files.
+
+    Args:
+        app (FivePaisaWrapper): The FivePaisaWrapper object.
+        totp (str): The TOTP (Time-based One-Time Password) for authentication.
+        save_location (str): The location to save the updated data.
+        data_read_location (str): The location to read the existing data.
+        intraday (bool, optional): Whether to update intraday data. Default is True.
+        updateFrom (datetime.datetime, optional): The date and time from which to update the data. Default is None.
+            If not provided, the function will check the latest available data and start from there.
+
+    Returns:
+        dict: A dictionary containing the updated data for each symbol.
+    """
     app.login(totp)
-    app.load_conv_dict(
-        "/home/yeashu/project/AlgoTrading app/scrips/symbols2Scip.csv"
-    )
-    csvFilepath = "/home/yeashu/project/AlgoTrading app/nifty_data_download/Data/Equities_csv/daily/5p"
-    timeIndex = "Datetime"
-    if intraday:
-        csvFilepath = "/home/yeashu/project/AlgoTrading app/nifty_data_download/Data/Equities_csv/intraday"
-        timeIndex = "Datetime"
-    
-    if not updateFrom:
+    app.load_conv_dict(data_read_location)
+
+    timeIndex = "Datetime" if intraday else "Date"
+
+    data = {}
+    csvFilepath = save_location
+
+    try:
         symbol = nifty50_stocks[37]
-        df = pd.read_csv(f'{csvFilepath}/{symbol}.csv')
+        df = load_dataframe_from_csv(symbol, csvFilepath, index_col=timeIndex)
         if timeIndex in df.columns:
             updateFrom = pd.to_datetime(df[timeIndex].iloc[-1]).to_pydatetime()
             print(updateFrom)
-    
-    # Set the start date for updating the data
-    start = updateFrom if updateFrom else datetime.datetime(2019, 1, 1)
-    end = datetime.datetime.now()  # Set the end date as the current date and time
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-    if intraday:
-        data = app.download_intraday_data(
-            nifty50_stocks, "15m", start=start, end=end, verbose=True
-        )
-    else :
-        data = app.download(nifty50_stocks,'1d',start.strftime("%Y-%m-%d"),end.strftime("%Y-%m-%d"))
+    start = updateFrom if updateFrom else datetime.datetime(2019, 1, 1)
+    end = datetime.datetime.now()
+
+    try:
+        if intraday:
+            data = app.download_intraday_data(
+                nifty50_stocks, "15m", start=start, end=end, verbose=True
+            )
+        else:
+            data = app.download(nifty50_stocks, '1d', start.strftime("%Y-%m-%d"), end.strftime("%Y-%m-%d"))
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
     for symbol, df in data.items():
         print(f"Saving updated {symbol} data to csv")
-        symbol_file = os.path.join(csvFilepath, f"{symbol}.csv")
-        existing_data = pd.read_csv(symbol_file,index_col=timeIndex) if os.path.isfile(symbol_file) else pd.DataFrame()
+        existing_data = load_dataframe_from_csv(symbol, csvFilepath, index_col=timeIndex)
         combined_data = pd.concat([existing_data, df])
         save_to_csv(combined_data, symbol, filepath=csvFilepath)
-    
+
     return data
-
-
-if __name__ == "__main__":
-    print("To update data enter 1 , To download data enter 2 and enter 0 to exit")
-    input = int(input(": "))
-    if input == 1:
-        UpdateData("Enter totp here")
-    elif input == 2:
-        app.login("Enter totp here")
-        app.load_conv_dict(
-            "/home/yeashu/project/AlgoTrading app/scrips/symbols2Scip.csv"
-        )
-
-        start = datetime.datetime(2019, 1, 1)
-        end = datetime.datetime(2023, 5, 25)
-        csvFilepath = (
-            "/home/yeashu/project/AlgoTrading app/nifty_data_download/Data/Equities_csv/intraday"
-        )
-
-        data = app.download_intraday_data(
-            nifty50_stocks, "15m", start=start, end=end, verbose=True
-        )
-
-        for symbol, df in data.items():
-            print(f"Saving {symbol} data to csv")
-            save_to_csv(df, symbol, filepath=csvFilepath)
